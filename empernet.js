6f8790174150fe9aae29332891c60f59869b582e
Empurion <58401258+Empurion@users.noreply.github.com>
2020-04-09 17:31:18 +0200

    Add files via upload

---
+++ empernet.js
@@ -0,0 +1,10 @@
+const mysql = require('mysql');
+const query = require('./query.js');
+//database
+
+//User Class
+class Empernet {
+  constructor(){
+  this.id = 470584611739795476;
+  }
+} module.exports = Empernet
